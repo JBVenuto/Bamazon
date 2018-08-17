1d7d1473f412f487976a049805b32e8cb3797f66
Joseph Venuto <jbvenuto@gmail.com>
2018-08-16 20:50:10 -0400

    added console.table npm and made questions to ask the user

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 //Require mysql and inquirer npm packages
 var inquirer = require("inquirer");
 var mysql = require("mysql");
+var cTable = require("console.table")
 
 //Connect to the database
 var connection = mysql.createConnection({
@@ -14,9 +15,26 @@ var connection = mysql.createConnection({
 function showTable() {
     connection.query("SELECT * FROM products", function(err, res) {
         if(err) throw err;
-        console.log(res);
+        console.table(res);
     });
+    promptUser();
 };
+
+function promptUser() {
+    inquirer.prompt([
+        //Ask the user the ID of the item they would like to buy
+        {
+            type: "input",
+            name: "idNumber",
+            message: "What is the id number of the product you would like to buy?"
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many would you like to buy?"
+        }
+    ])
+}
 
 //Call the function to show the user the merchandise
 showTable();
